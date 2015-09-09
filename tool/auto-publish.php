031ed2bc1c4784416f50e42f1cbe34581deaa5a9
lv-code <lvcode8@gmail.com>
2015-09-09 15:58:48 +0800

    add a tool file

---
+++ tool/auto-publish.php
@@ -0,0 +1,25 @@
+<?php
+set_time_limit(0);
+include(__DIR__.'/../wp-load.php');
+
+$n = rand(6,12);
+$args = array('post_status'=>'draft','numberposts'=>$n);
+$postArr = get_posts($args);
+//echo '<pre>';var_dump($postArr);
+// select ID from wp_POSTS where post_status='draft' limit 6;
+// update from wp_POSTS set post_status='public' where ID in ();
+
+if( !is_array($postArr) ) die('please check the $arr, is not Array type');
+foreach($postArr as $k=>$obj) {
+
+	$my_post = array(
+			'ID' => $obj->ID,
+			'post_status' => 'publish',
+			'post_date' => date('Y-m-d H:i:s'),
+			);
+	if( $my_post['ID']>0 ) {
+		var_export($my_post);die;
+		// update the post into the database
+		//wp_insert_post( $my_post );
+	}
+}
